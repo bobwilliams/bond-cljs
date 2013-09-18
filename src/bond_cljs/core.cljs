42e9ac206b14eb7efc445df926b9ac8a9d6182fe
Bryan Gilbert <gilbertw1@gmail.com>
2013-09-18 08:27:21 -0400

    Adding basic (hardcoded) configuration and updated less files

--- src/bond_cljs/core.cljs
+++ src/bond_cljs/core.cljs
@@ -2,22 +2,25 @@
 	(:require [bond-cljs.views :as views]
             [bond-cljs.chat :as chat]))
 
+;; TODO: From Config File
+(def config {:display-name "Skeezy McFly"
+             :accounts [{:user "mcskeezy@gmail.com"
+                         :pass "sUp3rS3Cr3t"
+                         :host "talk.google.com"
+                         :port 5222
+                         :reconnect true}]})
+
 ;; Render Initial Page
-(views/render-page (views/chat-page "gilbertw1" []))
+(views/render-page (views/chat-page (:display-name config) []))
 
 ;; Bind Page Events
 (views/bind-events)
 
-;; Connect Gtalk Account
-(def connection (chat/connect-account {:user "gilbertw1@gmail.com"
-                                       :pass "ulmwstqtalehpqct"
-                                       :host "talk.google.com"
-                                       :port 5222
-                                       :reconnect true}))
-
+;; Connect Accounts
+(def chat-connections (map chat/connect-account (:accounts config)))
 
 ;; Create Chat Event Observable
-(def chat-stream (chat/event-stream connection))
+(def chat-streams (map chat/event-stream chat-connections))
 
-;; React to chat event stream
-(views/bind-event-stream chat-stream)
+;; Bind Event Streams to UI
+(doseq [s chat-streams] (views/bind-event-stream s))
